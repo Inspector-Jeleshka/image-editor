cb0bdbfb270b054475d439f6226bf729e5de374e
Jeleshka <haidendark1236@gmail.com>
2024-12-26 19:33:05 +0700

    . + addTitle() for adding text on image # setPixel() more exceptions +editBrightness() for changing image brightness

--- ImageEditing/Image.cs
+++ ImageEditing/Image.cs
@@ -54,7 +54,7 @@
 
 		public int RowSize => (int)Math.Ceiling(header.bitsPerPixel * header.bmpWidth / 32d) * 4;
 
-		
+
 		public void Crop(int x1, int y1, int x2, int y2)
 		{
 			if (x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0)
@@ -66,7 +66,7 @@
 				throw new ArgumentException("Image can't have side with a 0 length");
 
 			Pixel[,] croppedImage = new Pixel[newImageHeight, newImageWidth];
-			x1 = x1 < x2 ? x1 :	x2;
+			x1 = x1 < x2 ? x1 : x2;
 			y1 = y1 < y2 ? y1 : y2;
 
 			for (int i = 0; i < newImageHeight; i++)
@@ -132,16 +132,56 @@
 			fileSize = pixelArrayOffset + header.bmpSize;
 		}
 
+		public void AddTitle(string title, int x, int y)
+		{
+			for (int i = 0; i < title.Length; i++)
+			{
+				char c = char.ToUpper(title[i]);
+				if (!Font.font.ContainsKey(c))
+					continue;
+
+				byte[] charBitmap = Font.font[c];
+				for (int row = 0; row < Font.charHeight; row++)
+					for (int col = 0; col < Font.charWidth; col++)
+					{
+						int pixelX = x + col + i * (Font.charWidth + 1);
+						int pixelY = y + row;
+						if (pixelX >= header.bmpWidth || pixelY >= header.bmpHeight || pixelX < 0 || pixelY < 0)
+							continue;
+
+						if ((charBitmap[row] & (1 << (Font.charWidth - 1 - col))) != 0 )
+							SetPixel(pixelX, pixelY, 0, 0, 0);
+					}
+			}
+		}
+
 		public void SetPixel(int x, int y, byte red, byte green, byte blue)
 		{
 			SetPixel(x, y, new(red, green, blue));
 		}
 		public void SetPixel(int x, int y, Pixel pixel)
 		{
-			if (x < 0 || y < 0)
-				throw new ArgumentOutOfRangeException(x < 0 ? nameof(x) : nameof(y), "Less than 0");
+			if (x < 0 || x >= header.bmpWidth)
+				throw new ArgumentOutOfRangeException(nameof(x), "Outside the pixel array");
+			if (y < 0 || y >= header.bmpHeight)
+				throw new ArgumentOutOfRangeException(nameof(y), "Outside the pixel array");
 
 			pixelArray[y, x] = pixel;
+		}
+
+		public void EditBrightness(int value)
+		{
+			for (int i = 0; i < header.bmpHeight; i++)
+				for (int j = 0; j < header.bmpWidth; j++)
+				{
+					int red = pixelArray[i, j].Red + value;
+					int green = pixelArray[i, j].Green + value;
+					int blue = pixelArray[i, j].Blue + value;
+
+					Func<int, byte> normalize = (value) => { if (value > 255) return 255; if (value < 0) return 0; return (byte)value; };
+
+					pixelArray[i, j] = new(normalize(red), normalize(green), normalize(blue));
+				}
 		}
 
 		public void SaveToFile()
@@ -233,6 +273,87 @@
 				this.verticalResolution = verticalResolution;
 				this.colorTableSize = colorTableSize;
 			}
+		}
+		private readonly struct Font
+		{
+			public static readonly int charWidth = 5;
+			public static readonly int charHeight = 7;
+			public static readonly Dictionary<char, byte[]> font = new()
+			{
+				{ 'A', new byte[] { 0b00100, 0b01010, 0b10001, 0b10001, 0b11111, 0b10001, 0b10001 } },
+				{ 'B', new byte[] { 0b11110, 0b10001, 0b10001, 0b11110, 0b10001, 0b10001, 0b11110 } },
+				{ 'C', new byte[] { 0b01110, 0b10001, 0b10000, 0b10000, 0b10000, 0b10001, 0b01110 } },
+				{ 'D', new byte[] { 0b11100, 0b10010, 0b10001, 0b10001, 0b10001, 0b10010, 0b11100 } },
+				{ 'E', new byte[] { 0b11111, 0b10000, 0b10000, 0b11110, 0b10000, 0b10000, 0b11111 } },
+				{ 'F', new byte[] { 0b11111, 0b10000, 0b10000, 0b11110, 0b10000, 0b10000, 0b10000 } },
+				{ 'G', new byte[] { 0b01110, 0b10001, 0b10000, 0b10000, 0b10011, 0b10001, 0b01110 } },
+				{ 'H', new byte[] { 0b10001, 0b10001, 0b10001, 0b11111, 0b10001, 0b10001, 0b10001 } },
+				{ 'I', new byte[] { 0b01110, 0b00100, 0b00100, 0b00100, 0b00100, 0b00100, 0b01110 } },
+				{ 'J', new byte[] { 0b00011, 0b00001, 0b00001, 0b00001, 0b00001, 0b10001, 0b01110 } },
+				{ 'K', new byte[] { 0b10001, 0b10010, 0b10100, 0b11000, 0b10100, 0b10010, 0b10001 } },
+				{ 'L', new byte[] { 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b11111 } },
+				{ 'M', new byte[] { 0b10001, 0b11011, 0b10101, 0b10101, 0b10001, 0b10001, 0b10001 } },
+				{ 'N', new byte[] { 0b10001, 0b10001, 0b11001, 0b10101, 0b10011, 0b10001, 0b10001 } },
+				{ 'O', new byte[] { 0b01110, 0b10001, 0b10001, 0b10001, 0b10001, 0b10001, 0b01110 } },
+				{ 'P', new byte[] { 0b11110, 0b10001, 0b10001, 0b11110, 0b10000, 0b10000, 0b10000 } },
+				{ 'Q', new byte[] { 0b01110, 0b10001, 0b10001, 0b10001, 0b10011, 0b10010, 0b01101 } },
+				{ 'R', new byte[] { 0b11110, 0b10001, 0b10001, 0b11110, 0b10100, 0b10010, 0b10001 } },
+				{ 'S', new byte[] { 0b01110, 0b10001, 0b10000, 0b01110, 0b00001, 0b10001, 0b01110 } },
+				{ 'T', new byte[] { 0b11111, 0b00100, 0b00100, 0b00100, 0b00100, 0b00100, 0b00100 } },
+				{ 'U', new byte[] { 0b10001, 0b10001, 0b10001, 0b10001, 0b10001, 0b10001, 0b01110 } },
+				{ 'V', new byte[] { 0b10001, 0b10001, 0b10001, 0b10001, 0b10001, 0b01010, 0b00100 } },
+				{ 'W', new byte[] { 0b10001, 0b10001, 0b10001, 0b10101, 0b10101, 0b11011, 0b10001 } },
+				{ 'X', new byte[] { 0b10001, 0b10001, 0b01010, 0b00100, 0b01010, 0b10001, 0b10001 } },
+				{ 'Y', new byte[] { 0b10001, 0b10001, 0b10001, 0b01110, 0b00100, 0b00100, 0b00100 } },
+				{ 'Z', new byte[] { 0b11111, 0b00001, 0b00010, 0b00100, 0b01000, 0b10000, 0b11111 } },
+
+				{ '0', new byte[] { 0b01110, 0b10001, 0b10011, 0b10101, 0b11001, 0b10001, 0b01110 } },
+				{ '1', new byte[] { 0b00100, 0b01100, 0b00100, 0b00100, 0b00100, 0b00100, 0b01110 } },
+				{ '2', new byte[] { 0b01110, 0b10001, 0b00001, 0b00110, 0b01000, 0b10000, 0b11111 } },
+				{ '3', new byte[] { 0b01110, 0b10001, 0b00001, 0b00110, 0b00001, 0b10001, 0b01110 } },
+				{ '4', new byte[] { 0b00010, 0b00110, 0b01010, 0b10010, 0b11111, 0b00010, 0b00010 } },
+				{ '5', new byte[] { 0b11111, 0b10000, 0b11110, 0b00001, 0b00001, 0b10001, 0b01110 } },
+				{ '6', new byte[] { 0b01110, 0b10001, 0b10000, 0b11110, 0b10001, 0b10001, 0b01110 } },
+				{ '7', new byte[] { 0b11111, 0b00001, 0b00010, 0b00100, 0b00100, 0b00100, 0b00100 } },
+				{ '8', new byte[] { 0b01110, 0b10001, 0b10001, 0b01110, 0b10001, 0b10001, 0b01110 } },
+				{ '9', new byte[] { 0b01110, 0b10001, 0b10001, 0b01111, 0b00001, 0b10001, 0b01110 } },
+
+				{ 'А', new byte[] { 0b00100, 0b01010, 0b10001, 0b10001, 0b11111, 0b10001, 0b10001 } },
+				{ 'Б', new byte[] { 0b11111, 0b10000, 0b11110, 0b10001, 0b10001, 0b10001, 0b11110 } },
+				{ 'В', new byte[] { 0b11110, 0b10001, 0b10001, 0b11110, 0b10001, 0b10001, 0b11110 } },
+				{ 'Г', new byte[] { 0b11111, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000 } },
+				{ 'Д', new byte[] { 0b01110, 0b01010, 0b01010, 0b01010, 0b11111, 0b10001, 0b10001 } },
+				{ 'Е', new byte[] { 0b11111, 0b10000, 0b10000, 0b11110, 0b10000, 0b10000, 0b11111 } },
+				{ 'Ж', new byte[] { 0b10001, 0b10101, 0b10101, 0b01110, 0b10101, 0b10101, 0b10001 } },
+				{ 'З', new byte[] { 0b01110, 0b10001, 0b00001, 0b00110, 0b00001, 0b10001, 0b01110 } },
+				{ 'И', new byte[] { 0b10001, 0b10001, 0b10011, 0b10101, 0b11001, 0b10001, 0b10001 } },
+				{ 'Й', new byte[] { 0b10101, 0b10001, 0b10011, 0b10101, 0b11001, 0b10001, 0b10001 } },
+				{ 'К', new byte[] { 0b10001, 0b10010, 0b10100, 0b11000, 0b10100, 0b10010, 0b10001 } },
+				{ 'Л', new byte[] { 0b01111, 0b01001, 0b01001, 0b01001, 0b01001, 0b10001, 0b10001 } },
+				{ 'М', new byte[] { 0b10001, 0b11011, 0b10101, 0b10101, 0b10001, 0b10001, 0b10001 } },
+				{ 'Н', new byte[] { 0b10001, 0b10001, 0b10001, 0b11111, 0b10001, 0b10001, 0b10001 } },
+				{ 'О', new byte[] { 0b01110, 0b10001, 0b10001, 0b10001, 0b10001, 0b10001, 0b01110 } },
+				{ 'П', new byte[] { 0b11111, 0b10001, 0b10001, 0b10001, 0b10001, 0b10001, 0b10001 } },
+				{ 'Р', new byte[] { 0b11110, 0b10001, 0b10001, 0b11110, 0b10000, 0b10000, 0b10000 } },
+				{ 'С', new byte[] { 0b01110, 0b10001, 0b10000, 0b10000, 0b10000, 0b10001, 0b01110 } },
+				{ 'Т', new byte[] { 0b11111, 0b00100, 0b00100, 0b00100, 0b00100, 0b00100, 0b00100 } },
+				{ 'У', new byte[] { 0b10001, 0b10001, 0b10001, 0b01111, 0b00001, 0b10001, 0b01110 } },
+				{ 'Ф', new byte[] { 0b00100, 0b01110, 0b10101, 0b10101, 0b10101, 0b01110, 0b00100 } },
+				{ 'Х', new byte[] { 0b10001, 0b10001, 0b01010, 0b00100, 0b01010, 0b10001, 0b10001 } },
+				{ 'Ц', new byte[] { 0b10010, 0b10010, 0b10010, 0b10010, 0b10010, 0b11111, 0b00001 } },
+				{ 'Ч', new byte[] { 0b10001, 0b10001, 0b10001, 0b01111, 0b00001, 0b00001, 0b00001 } },
+				{ 'Ш', new byte[] { 0b10101, 0b10101, 0b10101, 0b10101, 0b10101, 0b10101, 0b11111 } },
+				{ 'Щ', new byte[] { 0b10101, 0b10101, 0b10101, 0b10101, 0b10101, 0b11111, 0b00001 } },
+				{ 'Ъ', new byte[] { 0b11000, 0b01000, 0b01110, 0b01001, 0b01001, 0b01001, 0b01110 } },
+				{ 'Ы', new byte[] { 0b10001, 0b10001, 0b10001, 0b11101, 0b10011, 0b10011, 0b11101 } },
+				{ 'Ь', new byte[] { 0b10000, 0b10000, 0b10000, 0b11110, 0b10001, 0b10001, 0b11110 } },
+				{ 'Э', new byte[] { 0b01110, 0b10001, 0b00001, 0b01111, 0b00001, 0b10001, 0b01110 } },
+				{ 'Ю', new byte[] { 0b10110, 0b10101, 0b10101, 0b11101, 0b10101, 0b10101, 0b10110 } },
+				{ 'Я', new byte[] { 0b01110, 0b10001, 0b10001, 0b01111, 0b00101, 0b01001, 0b10001 } },
+			};
+
+			public Font()
+			{ }
 		}
 	}
 }
